538ee21c45448f4ed6aadc57a9f86c117866e341
Harsh Shah <harsh-bhavesh.shah@etu.univ-amu.fr>
2026-06-17 13:06:48 +0000

    Exercice 6 termine

--- src/main/java/fr/univ_amu/iut/exercice6/Exercice6Module.java
+++ src/main/java/fr/univ_amu/iut/exercice6/Exercice6Module.java
@@ -1,6 +1,7 @@
 package fr.univ_amu.iut.exercice6;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
 
 /**
  * Module Guice de l'exercice 6.
@@ -16,9 +17,9 @@ public class Exercice6Module extends AbstractModule {
 
   @Override
   protected void configure() {
-    // TODO exercice 6 : lier chaque implémentation de Notifieur à un nom.
-    //
-    // - le nom "console"    -> NotifieurConsole
+    bind(Notifieur.class).annotatedWith(Names.named("console")).to(NotifieurConsole.class);
+    bind(Notifieur.class).annotatedWith(Names.named("silencieux")).to(NotifieurSilencieux.class);
+    // - le nom "console" -> NotifieurConsole
     // - le nom "silencieux" -> NotifieurSilencieux
     // Astuce :
     // bind(Notifieur.class).annotatedWith(Names.named("console")).to(NotifieurConsole.class);
